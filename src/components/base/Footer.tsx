79534d2f509751ff4278445fd4d8b0ea8612e387
takeshi suzuki <takeshi.suzuki@n-code.ai>
2020-04-10 21:25:35 +0900

    modify readme

--- src/components/base/Footer.tsx
+++ src/components/base/Footer.tsx
@@ -6,8 +6,8 @@ function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {"Copyright © "}
-      <MuiLink color="inherit" href="https://n-code.ai/">
-        Labelly
+      <MuiLink color="inherit" href="https://example.com/">
+        FastLabel
       </MuiLink>{" "}
       {new Date().getFullYear()}
       {"."}
